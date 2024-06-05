ac2629db3b22ff348d5f6b6542b5023f714967f0
Justin Bennett <justin@membrane.io>
2024-06-05 14:51:40 +0200

    Add initial programs

---
+++ height-ops/index.ts
@@ -0,0 +1,29 @@
+import { root, nodes } from "membrane";
+
+export const email: resolvers.Root["email"] = async ({
+  from,
+  subject,
+  text,
+}) => {
+  const issues = subject.match(/T-\d+/g) || [];
+  if (issues.length === 0) return;
+
+  // Don't include issue prefix in subject
+  const lastIssue = issues.slice(-1)[0];
+  subject = subject.slice(subject.indexOf(lastIssue) + lastIssue.length).trim();
+
+  // Drop unsubscribe footer
+  text = text
+    .replace(
+      "To unsubscribe from this group and stop receiving emails from it, send an email to height+unsubscribe@membrane.io.",
+      ""
+    )
+    .trim();
+  text = `From: ${from}\nSubject: ${subject}\n\n${text}`;
+
+  for (const issue of issues) {
+    await nodes.height.tasks
+      .one({ id: issue.slice(2) })
+      .postComment({ message: text });
+  }
+};
