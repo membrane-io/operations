c708e2fce5885b7857a8644badd210a36957aa13
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2024-06-18 13:28:22 -0400

    Add /upvote and /downvote routes

--- public-roadmap/index.ts
+++ public-roadmap/index.ts
@@ -1,11 +1,14 @@
-import { nodes } from "membrane";
+import { state, nodes } from "membrane";
 
-// custom statuses return their id from the Height API
-const HEIGHT_BLOCKED_STATUS = "b7217600-255f-4ed6-b8ca-e8d2b7f6edc1";
+export interface State {
+  tasks: Record<string, Record<string, boolean>>;
+}
 
 function formatStatus(heightStatus?: string) {
-  let status = "";
+  // custom statuses return their id from the Height API
+  const HEIGHT_BLOCKED_STATUS = "b7217600-255f-4ed6-b8ca-e8d2b7f6edc1";
 
+  let status = "";
   switch (heightStatus) {
     case "backLog":
     case HEIGHT_BLOCKED_STATUS:
@@ -29,7 +32,26 @@ function formatDescription(heightDesc?: string) {
   return publicDescription;
 }
 
-export const endpoint: resolvers.Root["endpoint"] = async (req) => {
+function fetchUpvotes({
+  ipAddress,
+  taskId,
+}: {
+  ipAddress: string;
+  taskId?: string;
+}) {
+  if (!taskId) return { numUpvotes: 0, hasUpvoted: false };
+
+  if (!state.tasks[taskId]) {
+    state.tasks[taskId] = {};
+  }
+
+  const numUpvotes = Object.keys(state.tasks[taskId]).length;
+  const hasUpvoted = state.tasks[taskId][ipAddress] || false;
+
+  return { numUpvotes, hasUpvoted };
+}
+
+async function fetchTasks(ipAddress: string) {
   const fields = "{ id name description status }";
   const tasks = await nodes.tasks.items.$query(fields);
 
@@ -39,18 +61,37 @@ export const endpoint: resolvers.Root["endpoint"] = async (req) => {
       const { id, name, description: heightDesc, status: heightStatus } = task;
       const status = formatStatus(heightStatus);
       const description = formatDescription(heightDesc);
+      const { numUpvotes, hasUpvoted } = fetchUpvotes({
+        ipAddress,
+        taskId: id,
+      });
 
-      return { id, name, description, status };
+      return { id, name, description, status, numUpvotes, hasUpvoted };
     });
 
+  return publicRoadmapTasks;
+}
+
+export const endpoint: resolvers.Root["endpoint"] = async (req) => {
   switch (`${req.method} ${req.path}`) {
     case "GET /":
-      const body = JSON.stringify(publicRoadmapTasks);
+      const ip = JSON.parse(req.headers)["x-forwarded-for"];
+
       return JSON.stringify({
         status: 200,
         headers: { "Content-Type": "application/json" },
-        body,
+        body: JSON.stringify(await fetchTasks(ip)),
       });
+    case "POST /upvote":
+    case "POST /downvote":
+      const { id } = JSON.parse(req.body as string);
+      const ipAddress = JSON.parse(req.headers)["x-forwarded-for"];
+
+      req.path.includes("upvote")
+        ? (state.tasks[id][ipAddress] = true)
+        : delete state.tasks[id][ipAddress];
+
+      return JSON.stringify({ status: 201 });
     default:
       return JSON.stringify({ status: 404 });
   }
